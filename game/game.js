22c63bbdd74f5f1eddba130ce85fb181bc190a1d
meanochi <michal.icecream@gmail.com>
2025-07-21 20:38:50 +0300

    hi

---
+++ game/game.js
@@ -0,0 +1,331 @@
+//////////////////////
+//הערות:
+//1 אם אין אפשרות לזוז לכיוון שלא יגרילvvvv
+//2 אם הלוח מלא ויש להתקדם נתקעVVVV
+//3 לא קולט שמנצחvvvv
+//4 לשנות צבע לפי ערךvvvv
+////////////////////////
+const sqrs1=document.querySelectorAll(".r1")
+const sqrs2=document.querySelectorAll(".r2")
+const sqrs3=document.querySelectorAll(".r3")
+const sqrs4=document.querySelectorAll(".r4")
+const hello = document.querySelector(".hello")
+const scoreDiv=document.querySelector(".score")
+const logOut = document.querySelector("#logOut")
+const home = document.querySelector("#home")
+const sqrs=[sqrs1,sqrs2,sqrs3,sqrs4]
+console.log(sqrs);
+let colors={0:"transparent",2:"lightgoldenrodyellow",4:"lightblue",8:"lightgray",16:"lightgreen",32:"lightpink",64:"lightsalmon",128:"lightblue",256:"lightseagreen",512:"lightslategray",1024:"lightcoral",2048:"goldenrod"}
+let countSqrs=0
+let score=0
+let flag=true
+userName = localStorage.getItem('loggedInUser')
+hello.textContent = userName
+home.addEventListener("click", ()=>{
+    window.location.href=`../home/home.html?action=checkLoginStatus())`
+})
+
+logOut.addEventListener("click",(event)=>{
+    event.preventDefault()
+    localStorage.setItem('loggedInUser', '')
+    window.location.href=`../home/home.html?action=checkLoginStatus())`
+})
+console.log(userName);
+function twoOrFour(){
+    const arr=[4,2,2,2,2,2,2,2,2,2]
+    const index=Math.floor(Math.random()*10)
+    return arr[index]
+}
+
+function changeColor(sqrs){
+    console.log(colors);
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+            if(sqrs[i][j].textContent===''){
+                sqrs[i][j].style.backgroundColor=colors[0]
+            }
+            else{
+                sqrs[i][j].style.backgroundColor=colors[Number(sqrs[i][j].textContent)]
+            }
+        }
+    }
+}
+
+function canMove(sqrs){
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 3; j++) {
+            if(sqrs[i][j].textContent===sqrs[i][j+1].textContent)
+                return true
+        }
+    }
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 4; j++) {
+            if(sqrs[j][i].textContent===sqrs[j][i+1].textContent)
+                return true
+        }
+    }
+    return false
+}
+
+function randomSqr(sqrs){
+    const empty=[]
+    for(let i=0;i<4;i++){
+        for(let j=0;j<4;j++){
+            if(sqrs[i][j].textContent===''){
+                empty.push(sqrs[i][j])
+            }
+        }
+    }
+    let num = Math.floor(Math.random() * empty.length)
+    const place = empty[num]
+    place.textContent = twoOrFour()
+}
+
+function start(sqrs) {
+    countSqrs+=2
+    randomSqr(sqrs)
+    randomSqr(sqrs)
+    changeColor(sqrs)
+}
+
+function IsGameOver(sqrs){
+    const empty=[]
+    for(let i=0;i<4;i++){
+        for(let j=0;j<4;j++){
+            if(sqrs[i][j].textContent===''){
+                empty.push(sqrs[i][j])
+            }
+        }
+    }
+    if(empty.length===0 && !canMove(sqrs)){
+        user = JSON.parse(localStorage.getItem(userName))
+        if(score>user["score"]){
+            user["score"] = score
+            localStorage.setItem(userName, JSON.stringify(user))
+        }
+         if(score>localStorage.getItem("high_score"))
+            localStorage.setItem("high_score", score)
+        alert("Game Over!!🤔 Your score is:"+score)
+        //איפוס מחדש
+        start(sqrs)
+    }
+}
+
+function winner(sqrs){
+    for(let i=0;i<4;i++){
+        for(let j=0;j<4;j++){
+            if(Number(sqrs[i][j].textContent)===2048){
+                alert("You are the winner!!!!!!!!!!👍👍👍 Your score is:"+score+"\n"+"🎉🎉🎉🎉🎉🎉")
+                return
+            }
+        }
+    }
+}
+
+
+function fixRight(sqrs){
+    countSqrs+=1
+    for(let i=0;i<4;i++){
+        let tmp=[]
+        let k=0
+        if(sqrs[i][0].textContent!==''){
+            tmp[0]=sqrs[i][0].textContent
+            k++
+        }
+        for(let j=1;j<4;j++){
+            if (sqrs[i][j].textContent==='')
+                continue
+            if(sqrs[i][j].textContent===tmp[k-1]){
+                tmp[k-1]*=2
+                tmp[k]=-1
+                score+=tmp[k-1]
+                console.log(score);
+            }
+            else{
+                tmp[k]=sqrs[i][j].textContent
+            } 
+            k++
+        }
+        let index=k-1
+        for (let j=3;j>=0;j--){
+            if(index<0){
+                sqrs[i][j].textContent=''
+            }
+            else if(tmp[index]===-1){
+                index--
+                sqrs[i][j].textContent=tmp[index]
+            }
+            else{
+                sqrs[i][j].textContent=tmp[index]
+            }
+            index--
+        }
+    }
+}
+
+function fixLeft(sqrs){
+    countSqrs+=1
+    for(let i=0;i<4;i++){
+        let tmp=[]
+        let k=0
+        if(sqrs[i][0].textContent!==''){
+            tmp[0]=sqrs[i][0].textContent
+            k++
+        }
+        for(let j=1;j<4;j++){
+            if (sqrs[i][j].textContent==='')
+                continue
+            if(Number(sqrs[i][j].textContent)===Number(tmp[k-1])){
+                tmp[k-1]*=2
+                tmp[k]=-1
+                score+=tmp[k-1]
+                console.log(score);
+            }
+            else{
+                tmp[k]=sqrs[i][j].textContent
+            } 
+            k++
+        }
+        let index=0
+        for (let j=0;j<4;j++){
+            if(index>=k){
+                sqrs[i][j].textContent=''
+            }
+            else if(tmp[index]===-1){
+                index++
+                sqrs[i][j].textContent=tmp[index]
+            }
+            else{
+                sqrs[i][j].textContent=tmp[index]
+            }
+            index++
+        }
+    }
+}
+
+function fixUp(sqrs){
+    countSqrs+=1
+    for(let i=0;i<4;i++){
+        let tmp=[]
+        let k=0
+        if(sqrs[0][i].textContent!==''){
+            tmp[0]=sqrs[0][i].textContent
+            k++
+        }
+        for(let j=1;j<4;j++){
+            if (sqrs[j][i].textContent==='')
+                continue
+            if(sqrs[j][i].textContent===tmp[k-1]){
+                tmp[k-1]*=2
+                tmp[k]=-1
+                score+=tmp[k-1]
+                console.log(score);
+            }
+            else{
+                tmp[k]=sqrs[j][i].textContent
+            } 
+            k++
+        }
+        let index=0
+        for (let j=0;j<4;j++){
+            if(index>=k){
+                sqrs[j][i].textContent=''
+            }
+            else if(tmp[index]===-1){
+                index++
+                sqrs[j][i].textContent=tmp[index]
+            }
+            else{
+                sqrs[j][i].textContent=tmp[index]
+            }
+            index++
+        }
+    }
+}
+
+function fixDown(sqrs){
+    for(let i=0;i<4;i++){
+        let tmp=[]
+        let k=0
+        if(sqrs[0][i].textContent!==''){
+            tmp[0]=sqrs[0][i].textContent
+            k++
+        }
+        for(let j=1;j<4;j++){
+            if (sqrs[j][i].textContent==='')
+                continue
+            if(sqrs[j][i].textContent===tmp[k-1]){
+                tmp[k-1]*=2
+                tmp[k]=-1
+                score+=tmp[k-1]
+            }
+            else{
+                tmp[k]=sqrs[j][i].textContent
+            } 
+            k++
+        }
+        let index=k-1
+        for (let j=3;j>=0;j--){
+            if(k<0){
+                sqrs[j][i].textContent=''
+            }
+            else if(tmp[index]===-1){
+                index--
+                sqrs[j][i].textContent=tmp[index]
+            }
+            else{
+                sqrs[j][i].textContent=tmp[index]
+            }
+            index--
+        }
+    }
+}
+let before=[]
+function saveBefore(sqrs){
+    before=[]
+    for(let i=0;i<4;i++){
+        before[i]=[]
+        for(let j=0;j<4;j++){
+            before[i][j]=sqrs[i][j].textContent
+        }
+    }
+}
+
+function hasChanged(sqrs){
+    for(let i=0;i<4;i++){
+        for(let j=0;j<4;j++){
+            if(sqrs[i][j].textContent!==before[i][j]){
+                return true
+            }
+        }
+    }
+    return false
+}
+
+document.addEventListener('keydown', function(event) {
+    saveBefore(sqrs)
+    switch(event.key) {
+      case "ArrowUp":
+        fixUp(sqrs)
+        break;
+      case "ArrowDown":
+        fixDown(sqrs)
+        break;
+      case "ArrowLeft":
+        fixLeft(sqrs)
+        break;
+      case "ArrowRight":
+        fixRight(sqrs)
+        break;
+    }
+
+    winner(sqrs)
+    IsGameOver(sqrs)
+    if (hasChanged(sqrs)) {
+        randomSqr(sqrs);
+    }
+    changeColor(sqrs)
+    scoreDiv.textContent="Score:"+score
+  });
+
+start(sqrs)
---
+++ home/home.js
@@ -0,0 +1,168 @@
+const register = document.querySelector(".reg")
+const login = document.querySelector(".log")
+const regiForm = document.querySelector(".register")
+const loginForm = document.querySelector(".login")
+const games = document.querySelector(".games")
+const signUp = document.querySelector("#toReg")
+const signIn = document.querySelector("#toSign")
+const logOut = document.querySelector("#logOut")
+const game = document.querySelector(".g2048")
+const hello = document.querySelector(".hello")
+const msg = document.querySelector(".msg")
+const passmsg1 = document.querySelector(".passmsg1")
+const passmsg2 = document.querySelector(".passmsg2")
+const h1 = document.querySelector(".h1")
+console.log(hello) 
+let isIn = true
+let name1 = ''
+let score = 0
+let userName = '' 
+if(!localStorage.getItem("high_score")){
+    localStorage.setItem("high_score",0)
+}
+function checkLoginStatus() {
+    userName = localStorage.getItem('loggedInUser')
+    if (userName) {
+        hello.textContent = `Hello ${userName}!` 
+        console.log(JSON.parse(localStorage.getItem(`${userName}`)));
+        
+        game.textContent = `Highest score:${localStorage.getItem("high_score")}\n Your score: ${JSON.parse(localStorage.getItem(`${userName}`))["score"]}`
+        signIn.style.display = "none" 
+        signUp.style.display = "none" 
+        logOut.style.display = "inline-block"
+        games.style.display = "flex"  
+        regiForm.style.display = "none" 
+        loginForm.style.display = "none" 
+    } else {
+        game.textContent = "Log in first"
+        logOut.style.display = 'none'
+        hello.textContent = "" 
+        signIn.style.display = "inline-block"
+        signUp.style.display = "inline-block" 
+        games.style.display = "flex"
+        regiForm.style.display = "none"
+        loginForm.style.display = "none"
+    }
+}
+function checkPass(pass){
+    let digit = false
+    let char = false
+    if(pass.length < 6)
+        return false
+    console.log(pass.length) 
+    for (let i = 0; i < pass.length; i++) {
+        console.log(pass[i]) 
+        console.log(isNaN(Number(pass[i]))) 
+        console.log(!isNaN(Number(pass[i]))) 
+        if(isNaN(Number(pass[i])))
+            digit = true
+        if(!isNaN(Number(pass[i])))
+            char = true
+        if(digit && char)
+            return true
+    }
+    return false        
+}
+register.addEventListener("click",(event)=>{
+    event.preventDefault()
+    const f_name = document.querySelector("#f_name").value
+    const phone = document.querySelector("#phone").value
+    const email = document.querySelector("#email").value
+    const password = document.querySelector("#password").value
+    regiForm.reset()
+    if(localStorage.getItem(f_name))
+        passmsg1.textContent = "There is a user with this name"
+    else if (checkPass(password)){
+        const user = {
+            f_name: f_name,
+            phone: phone, 
+            email: email, 
+            password: password, 
+            score: 0, 
+            tries: 0
+        }
+        localStorage.setItem(f_name, JSON.stringify(user))
+        localStorage.setItem('loggedInUser', f_name)
+        checkLoginStatus()
+    }
+    else{
+        passmsg1.textContent = "Password must contain at least one letter and one digit, altogather: 6 chars"
+        regiForm.style.display = 'flex'
+        games.style.display = 'none'
+        loginForm.style.display = 'none'
+        password.value = ''
+    }
+
+})
+console.log(name1) 
+
+logOut.addEventListener("click",(event)=>{
+    event.preventDefault()
+    localStorage.setItem('loggedInUser', '')
+    checkLoginStatus()
+})
+
+login.addEventListener("click",(event)=>{
+    event.preventDefault()
+    const f_name = document.querySelector("#f_name2").value
+    const password = document.querySelector("#password2").value
+    pass = JSON.parse(localStorage.getItem(f_name))
+    loginForm.reset()
+    if(!pass){
+        loginForm.reset()
+        passmsg2.textContent = "There is not a user in this name"
+    }
+    else if(password === pass["password"]){
+        localStorage.setItem('loggedInUser', f_name) 
+        checkLoginStatus()
+    }
+    else{
+       let tries = pass["tries"]
+       tries++
+       if(tries === 3){
+        loginForm.reset()
+        passmsg2.textContent = "You tried too many times with wrong password. your user had blocked. please register"
+        localStorage.removeItem(f_name)
+       }
+       else{
+        pass["tries"] = tries
+        localStorage.setItem(f_name, JSON.stringify(pass))
+        passmsg2.textContent = `Password is not correct. you have ${3-tries} more tries`
+    }
+    }
+
+
+})
+
+signIn.addEventListener("click", ()=>{
+    loginForm.style.display = 'flex'
+    signUp.style.borderBottom = ''
+    signIn.style.borderBottom = 'solid 2px'
+    games.style.display = 'none'
+    regiForm.style.display = 'none'
+})
+
+signUp.addEventListener("click", ()=>{
+    signUp.style.borderBottom = 'solid 2px'
+    signIn.style.borderBottom = ''
+    regiForm.style.display = 'flex'
+    games.style.display = 'none'
+    loginForm.style.display = 'none'
+
+})
+
+game.addEventListener("click",()=>{
+    userName = localStorage.getItem('loggedInUser') 
+    if (userName) {
+        window.location.href="../game/game.html?"
+        game.style.backgroundColor = 'red'
+    }
+    else{
+    loginForm.style.display = 'flex'
+    games.style.display = 'none'
+    regiForm.style.display = 'none'  
+    }
+})
+
+checkLoginStatus() 
+
